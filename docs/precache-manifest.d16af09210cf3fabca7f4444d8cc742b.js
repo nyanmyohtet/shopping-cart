f78bb20561d228cd1471846b5901811fd57ff202
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2020-01-09 21:40:38 +0630

    Test depoly

---
+++ docs/precache-manifest.d16af09210cf3fabca7f4444d8cc742b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674e4fdbe2dce47ca38447d16180dd07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "967999c1a53fa8ced32e",
+    "url": "/static/css/main.e8963b87.chunk.css"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "967999c1a53fa8ced32e",
+    "url": "/static/js/main.0d72ce02.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
